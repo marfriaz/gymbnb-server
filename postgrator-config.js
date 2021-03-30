910f3d2cd9c32e9f732a646a2c1b3782b763b904
Marco Friaz <marfriaz@gmail.com>
2021-03-29 22:23:27 -0700

    redeploy to heroku

--- postgrator-config.js
+++ postgrator-config.js
@@ -7,4 +7,5 @@ module.exports = {
     process.env.NODE_ENV === "test"
       ? process.env.TEST_DATABASE_URL
       : process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false, strictSSL: false },
 };
