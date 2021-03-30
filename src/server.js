f680dcbc92ad474d150fdf02ae34842499dff47d
Marco Friaz <marfriaz@gmail.com>
2021-03-29 22:35:54 -0700

    SSL fix

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const knex = require("knex");
 const app = require("./app");
 const { PORT, DATABASE_URL } = require("./config");
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 const db = knex({
   client: "pg",
   connection: DATABASE_URL,
