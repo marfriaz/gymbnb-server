5872cd3179e20f773eccc68acbeafc0040f2ce6d
Marco Friaz <marfriaz@gmail.com>
2021-03-30 21:26:44 -0700

    aws support

--- src/gyms/gyms-router.js
+++ src/gyms/gyms-router.js
@@ -2,15 +2,35 @@ const express = require("express");
 const path = require("path");
 const GymsService = require("./gyms-service");
 const { requireAuth } = require("../middleware/jwt-auth");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const AWS = require("aws-sdk");
+const { v4: uuidv4 } = require("uuid");
 
 const gymsRouter = express.Router();
 const jsonBodyParser = express.json();
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ID,
+  secretAccessKey: process.env.AWS_SECI,
+});
+
+const uploadS3 = multer({
+  storage: multerS3({
+    s3: s3,
+    acl: "public-read",
+    bucket: "gymbnb",
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    key: function (req, file, cb) {
+      cb(null, `${uuidv4()}.jpeg`);
+    },
+  }),
+}).array("image", 6);
 
 gymsRouter
   .route("/")
 
   .get((req, res, next) => {
-    console.log("demo");
     GymsService.getAllGyms(req.app.get("db"))
       .then((gyms) => {
         res.json(gyms.map(GymsService.serializeGym));
@@ -18,17 +38,11 @@ gymsRouter
       .catch(next);
   })
 
-  .post(requireAuth, jsonBodyParser, (req, res, next) => {
-    const {
-      title,
-      description,
-      max_guest,
-      location,
-      price,
-      img_urls,
-    } = req.body;
+  .post(requireAuth, jsonBodyParser, uploadS3, (req, res, next) => {
+    let imgs = req.files.map((str) => str.location);
 
-    const imgs = img_urls;
+    const { title, description, max_guest, location, price } = req.body;
+
     const newGym = {
       title,
       description,
@@ -43,11 +57,10 @@ gymsRouter
           error: `Missing '${key}' in request body`,
         });
 
-    newGym.user_id = req.user.id;
+    newGym.user_id = 1;
 
-    GymsService.insertGym(req.app.get("db"), newGym, imgs)
+    GymsService.insertGym(req.app.get("db"), newGym, `{${imgs}}`)
       .then((gym) => {
-        console.log(gym);
         res
           .status(201)
           .location(path.posix.join(req.originalUrl, `/${gym.id}`))
@@ -59,7 +72,6 @@ gymsRouter
 gymsRouter
   .route("/:gym_id")
   .all(checkGymIdExists)
-  //the async function has not been finished yet
   .get((req, res) => {
     res.json(GymsService.serializeGym(res.gym));
   });
@@ -70,16 +82,11 @@ gymsRouter
   .all(checkGymLocationExists)
   .get((req, res) => {
     const results = res.gyms.map((gym) => GymsService.serializeGym(gym));
-    console.log(results);
     res.send(results);
   });
 
-/* async/await syntax for promises */
 async function checkGymIdExists(req, res, next) {
   try {
-    // suspends execution of rest of function until promise is fulfilled or rejected
-    // and it yields the control back to where the async function was called
-    // if waiting for that
     const gym = await GymsService.getById(req.app.get("db"), req.params.gym_id);
 
     if (!gym)
